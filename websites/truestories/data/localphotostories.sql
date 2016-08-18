d3bfcc6fc81322a3aedaabf81d7291fd8bfcd7cb
David Ma <github@david-ma.net>
2016-08-19 03:11:21 +1000

    Added the truestories project (GovHack 2015)

---
+++ websites/truestories/data/localphotostories.sql
@@ -0,0 +1,16 @@
+create table localphotostories (
+	Title varchar(256),
+	URL varchar(256),
+	Date date,
+	Primary_image varchar(256),
+	Primary_image_caption varchar(256),
+	Primary_image_rights_information varchar(256),
+	Subjects varchar(256),
+	Station varchar(256),
+	State varchar(256),
+	Place varchar(256),
+	Keywords varchar(256),
+	Latitude float,
+	Longitude float,
+	MediaRSS_URL varchar(256)
+);

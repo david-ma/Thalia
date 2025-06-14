0d0b2fe2307bd72a5d088c2d6c4232587a49af78
David Ma <hello@david-ma.net>
2025-06-15 03:02:44 +1000

    Add ecosystem.config.json so we can use pm2

--- ecosystem.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  apps: [
-    {
-      name: 'thalia',
-      script: 'server/thalia.js',
-      log_file: `${__dirname}/websites/default/public/log/log-${new Date().toISOString().slice(0, 10)}.txt`,
-    },
-  ],
-}

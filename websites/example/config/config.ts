c15e6c1a4839b90a80269fbdcf93bbae10d7acc1
David Ma <hello@david-ma.net>
2025-06-15 18:11:49 +1000

    Simplify the example webpage a lot

--- websites/example/config/config.ts
+++ websites/example/config/config.ts
@@ -1,30 +1,3 @@
-// import { users, SecurityOptions } from 'thalia/server'
-
-// // import { cred } from './cred.js'
-
-// const securityOptions: SecurityOptions = {
-//   websiteName: 'default',
-//   mailFrom: 'Thalia <thalia@david-ma.net>',
-//   mailAuth: {
-//     user: 'user@david-ma.net',
-//     pass: 'password',
-//   },
-// }
-
-// export const config = {
-//   domains: [],
-//   data: false,
-//   dist: false,
-//   controllers: {
-//     ...users(securityOptions),
-//   },
-// }
-
-// import { latestlogs } from 'thalia/website'
-
 export const config = {
   domains: ['example.com'],
-  // controllers: {
-  //   latestlogs
-  // }
 }

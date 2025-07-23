e14e3504492bbb42b308cb00cc63bd324e137f89
David Ma <hello@david-ma.net>
2025-07-24 00:37:10 +1000

    Get drizzle to pull the database connection string straight out of the docker-compose.yml file

--- websites/example/drizzle.config.ts
+++ websites/example/drizzle.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from "drizzle-kit";
 
+// Read docker-compose.yml for the database credentials (written as a JSON object)
+import fs from 'fs'
+const dockerCompose = fs.readFileSync('docker-compose.yml', 'utf8')
+const dockerComposeJson = JSON.parse(dockerCompose)
+const { MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DATABASE } = dockerComposeJson.services.db.environment
+const MYSQL_PORT = dockerComposeJson.services.db.ports[0].split(':')[0]
+const url = `mysql://${MYSQL_USER}:${MYSQL_PASSWORD}@${MYSQL_HOST}:${MYSQL_PORT}/${MYSQL_DATABASE}`
+
 export default defineConfig({
   dialect: "mysql",
   schema: "./models/drizzle-schema.ts",
   out: "./drizzle",
-
-  dbCredentials: {
-    url: "mysql://not_root:password@localhost:3306/thalia",
-  }
+  dbCredentials: { url }
 });

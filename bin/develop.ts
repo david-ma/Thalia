bf02d046612433310398e5674f1b536fc1a18eb6
David Ma <hello@david-ma.net>
2025-12-01 23:19:58 +1100

    Get develop to work in stand-alone mode

--- bin/develop.ts
+++ bin/develop.ts
@@ -10,66 +10,110 @@ import readline from 'readline'
 import { getPort } from 'get-port-please'
 import { spinner } from '../server/util.js'
 
-const thaliaDirectory = process.cwd()
-
 // Get a port using get-port-please
 const preferredPort = 1337
 const port = await getPort({ port: preferredPort })
 
-// Get project name from command line arguments
+// This develop script can be run in two modes:
+// 1. From a project directory, with a websites directory
+// 2. From the root of a Thalia installation, with no websites directory
+// 2.1 If we're in the root of a Thalia installation, we might be passed a project name as an argument
+// 2.2 If we are not passed a project name, we will ask the user to select a project from the list of projects
+
+const rootPath = process.cwd()
+let thaliaDirectory = rootPath
+
+// Try to get project name from command line arguments
 let projectName = process.argv[2]
-const rawListOfProjects = fs
-  .readdirSync(path.resolve(thaliaDirectory, 'websites'))
-  .filter((file) => fs.statSync(path.resolve(thaliaDirectory, 'websites', file)).isDirectory())
-  .filter((file) => file !== 'default')
 
-const listOfProjects = rawListOfProjects
-
-if (projectName) {
-  if (!listOfProjects.includes(projectName)) {
-    console.error(`Project ${projectName} not found`)
-    process.exit(1)
-  }
-  startServer(projectName)
+// If there's no websites directory, we must be in standalone mode
+if (!fs.existsSync(path.join(rootPath, 'websites'))) {
+  console.log('No websites directory found')
+  thaliaDirectory = path.join(rootPath, 'node_modules', 'thalia')
+  projectName = path.basename(rootPath)
+  startServer({ projectName, projectRoot: rootPath, thaliaDirectory, standalone: true })
 } else {
-  // No project name provided? Ask for it
-  console.log('Available projects:')
-  console.log(listOfProjects.map((project, index) => `${index + 1}. ${project}`).join('\n'))
+  // If there's a websites directory, we're running from the Thalia directory
 
-  prompt('Enter the number of the project: ')
-    .then((projectNumber) => {
-      const projectNumberInt = parseInt(projectNumber)
-      if (isNaN(projectNumberInt) || projectNumberInt < 1 || projectNumberInt > listOfProjects.length) {
-        console.error('Please enter a valid project number')
-        process.exit(1)
-      }
-      return listOfProjects[projectNumberInt - 1]
-    })
-    .then((projectName) => {
-      if (!projectName) {
-        console.error('Please specify a project name')
-        process.exit(1)
-      }
-      console.log(`Starting server for project: ${projectName}\n`)
-      startServer(projectName)
-    })
+  const rawListOfProjects = fs
+    .readdirSync(path.resolve(thaliaDirectory, 'websites'))
+    .filter((file) => fs.statSync(path.resolve(thaliaDirectory, 'websites', file)).isDirectory())
+    .filter((file) => file !== 'default')
+
+  const listOfProjects = rawListOfProjects
+
+  if (projectName) {
+    if (!listOfProjects.includes(projectName)) {
+      console.error(`Project ${projectName} not found`)
+      process.exit(1)
+    }
+    startServer({ projectName, projectRoot: path.resolve(thaliaDirectory, 'websites', projectName), thaliaDirectory, standalone: false })
+  } else {
+    // No project name provided? Ask for it
+    console.log('Available projects:')
+    console.log(listOfProjects.map((project, index) => `${index + 1}. ${project}`).join('\n'))
+
+    prompt('Enter the number of the project: ')
+      .then((projectNumber) => {
+        const projectNumberInt = parseInt(projectNumber)
+        if (isNaN(projectNumberInt) || projectNumberInt < 1 || projectNumberInt > listOfProjects.length) {
+          console.error('Please enter a valid project number')
+          process.exit(1)
+        }
+        return listOfProjects[projectNumberInt - 1]
+      })
+      .then((projectName) => {
+        if (!projectName) {
+          console.error('Please specify a project name')
+          process.exit(1)
+        }
+        console.log(`Starting server for project: ${projectName}\n`)
+        startServer({
+          projectName,
+          projectRoot: path.resolve(thaliaDirectory, 'websites', projectName),
+          thaliaDirectory,
+          standalone: false,
+        })
+      })
+  }
 }
 
-function startServer(projectName: string) {
+function startServer({
+  projectName,
+  projectRoot,
+  thaliaDirectory,
+  standalone,
+}: {
+  projectName: string
+  projectRoot: string
+  thaliaDirectory: string
+  standalone: boolean
+}) {
   const env: NodeJS.ProcessEnv = {
     ...process.env,
     PORT: port.toString(),
     PROJECT: projectName,
   }
 
-  const projectRoot = path.resolve(thaliaDirectory, 'websites', projectName)
+  let server: ChildProcess
 
-  // Start Bun server with --watch (auto-restart on file changes)
-  const server = spawn('bun', ['--watch', 'server/cli.ts', `--project=${projectName}`], {
-    env,
-    stdio: 'inherit',
-    cwd: thaliaDirectory,
-  })
+  if (standalone) {
+      // Start Bun server with --watch (auto-restart on file changes)
+      server = spawn('bun', ['--watch', 'thalia'], {
+        env,
+        stdio: 'inherit',
+        cwd: projectRoot,
+      })
+
+  } else {
+    // Start Bun server with --watch (auto-restart on file changes)
+    server = spawn('bun', ['--watch', 'server/cli.ts', `--project=${projectName}`], {
+      env,
+      stdio: 'inherit',
+      cwd: thaliaDirectory,
+    })
+  }
+
 
   const processes: ChildProcess[] = [server]
 
